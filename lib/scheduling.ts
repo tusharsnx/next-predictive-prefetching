bd6825dd60967b8c2604ba3cd77e87337611f2ce
Tushar Singh <tusharsinghnx@gmail.com>
2025-04-12 13:34:36 +0000

    make scheduling funcs generics

--- lib/scheduling.ts
+++ lib/scheduling.ts
@@ -1,48 +1,51 @@
-export function debounced(fn: () => void, timeout: number) {
-    let timer: NodeJS.Timeout | null = null;
+export function debounced<TArgs extends unknown[]>(
+  fn: (...args: TArgs) => void,
+  timeout: number,
+) {
+  let timer: NodeJS.Timeout | null = null;
 
-    function clearTimer() {
-      if (!timer) return;
-      clearTimeout(timer);
-      timer = null;
-    }
-
-    function run() {
-      clearTimer();
-      fn();
-    }
-
-    return { run, clear: clearTimer };
+  function clearTimer() {
+    if (!timer) return;
+    clearTimeout(timer);
+    timer = null;
   }
 
-  export function throttled<Fn extends (...args: any[]) => void>(
-    fn: Fn,
-    interval: number,
-    runs: number,
-  ) {
-    let timer: NodeJS.Timeout | null = null;
-    let counter = runs;
-
-    function clearTimer() {
-      if (!timer) return;
-      clearTimeout(timer);
-      timer = null;
-    }
-
-    function queueResetCounter() {
-      if (timer) return;
-      timer = setTimeout(() => {
-        counter = runs;
-        timer = null;
-      }, interval);
-    }
-
-    function run(...args: Parameters<Fn>) {
-      if (counter <= 0) return;
-      fn(...args);
-      --counter;
-      queueResetCounter();
-    }
-
-    return { run, clear: clearTimer };
+  function run(...args: TArgs) {
+    clearTimer();
+    timer = setTimeout(() => fn(...args), timeout);
   }
+
+  return { run, clear: clearTimer };
+}
+
+export function throttled<TArgs extends unknown[]>(
+  fn: (...args: TArgs) => void,
+  interval: number,
+  runs: number,
+) {
+  let timer: NodeJS.Timeout | null = null;
+  let counter = runs;
+
+  function clearTimer() {
+    if (!timer) return;
+    clearTimeout(timer);
+    timer = null;
+  }
+
+  function queueResetCounter() {
+    if (timer) return;
+    timer = setTimeout(() => {
+      counter = runs;
+      timer = null;
+    }, interval);
+  }
+
+  function run(...args: TArgs) {
+    if (counter <= 0) return;
+    fn(...args);
+    --counter;
+    queueResetCounter();
+  }
+
+  return { run, clear: clearTimer };
+}
